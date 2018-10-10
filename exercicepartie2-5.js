13bc4402dea65c89f05394216cd397e642f78815
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-10 14:04:46 +0200

    first commit

---
+++ exercicepartie2-5.js
@@ -0,0 +1,29 @@
+//appel de l'Id, puis écoute des évenements 'passage de la souris' puis fonction
+document.getElementById("image1").addEventListener("mouseover",function(){
+//changement d'image
+  document.getElementById("image1").src="images/image1_2.jpg"
+})
+
+//appel de l'Id, puis écoute des évenements 'passage de la souris' puis fonction
+document.getElementById("image2").addEventListener("mouseover",function(){
+//changement d'image
+  document.getElementById("image2").src="images/image2_2.jpg"
+})
+
+//appel de l'Id, puis écoute des évenements 'passage de la souris' puis fonction
+document.getElementById("image3").addEventListener("mouseover",function(){
+//changement d'image
+  document.getElementById("image3").src="images/image3_2.jpg"
+})
+
+//appel de l'Id, puis écoute des évenements 'passage de la souris' puis fonction
+document.getElementById("image4").addEventListener("mouseover",function(){
+//changement d'image
+  document.getElementById("image4").src="images/image4_2.jpg"
+})
+
+//appel de l'Id, puis écoute des évenements 'passage de la souris' puis fonction
+document.getElementById("image5").addEventListener("mouseover",function(){
+//changement d'image
+  document.getElementById("image5").src="images/image5_2.jpg"
+})
